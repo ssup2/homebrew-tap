2e7bf9fc773794bf269ea957801c2237e944c03e
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-29 02:06:06 +0900

    Brew formula update for kpexec version v0.3.5

--- kpexec.rb
+++ kpexec.rb
@@ -5,32 +5,43 @@
 class Kpexec < Formula
   desc ""
   homepage ""
-  version "0.3.4"
-  bottle :unneeded
+  version "0.3.5"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.3.4/kpexec_v0.3.4_Darwin_amd64.tar.gz"
-      sha256 "757a9c4ab7c22d2e1f5aed21a04e4d5535d75fbf74a780afd0a8db1fd648400e"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.3.4/kpexec_v0.3.4_Darwin_arm64.tar.gz"
-      sha256 "adf994e03202b5912999c9035808b22ad4b5a81049015de6248fc70642ac026a"
+      url "https://github.com/ssup2/kpexec/releases/download/v0.3.5/kpexec_v0.3.5_Darwin_arm64.tar.gz"
+      sha256 "dc77f47a3c1f7cd535a05e9b6123b95bc7de77368a30d9394b372f0e1312e446"
+
+      def install
+        bin.install "kpexec"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ssup2/kpexec/releases/download/v0.3.5/kpexec_v0.3.5_Darwin_amd64.tar.gz"
+      sha256 "0b685d97f28cd5fdb2c66242c8e65d74cc3f4261cf2b1a803979899cad49f82f"
+
+      def install
+        bin.install "kpexec"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.3.4/kpexec_v0.3.4_Linux_amd64.tar.gz"
-      sha256 "6e534cead998582ab91c66fc4584b262010ccf2c9fff6e8f792d68f44277bea8"
-    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.3.4/kpexec_v0.3.4_Linux_arm64.tar.gz"
-      sha256 "7d0a56c251df784d72276e2df1f5c604c5b28e98236a05da85780d5b0a59498c"
-    end
-  end
+      url "https://github.com/ssup2/kpexec/releases/download/v0.3.5/kpexec_v0.3.5_Linux_arm64.tar.gz"
+      sha256 "8db395dfbbe858347c9b154bb6c19b3d3bf263c73bd36317525020020ed28370"
 
-  def install
-    bin.install "kpexec"
+      def install
+        bin.install "kpexec"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ssup2/kpexec/releases/download/v0.3.5/kpexec_v0.3.5_Linux_amd64.tar.gz"
+      sha256 "10111229fa749f72f12c44bba7facfef38d99999fa5b6ee95b585d8f36feabe7"
+
+      def install
+        bin.install "kpexec"
+      end
+    end
   end
 end
