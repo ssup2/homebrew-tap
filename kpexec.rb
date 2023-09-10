cdb97adb151ad5d8494e83f7846af376353e5f7d
goreleaserbot <goreleaser@carlosbecker.com>
2023-09-11 01:22:01 +0900

    Brew formula update for kpexec version v0.4.1

--- kpexec.rb
+++ kpexec.rb
@@ -5,20 +5,20 @@
 class Kpexec < Formula
   desc ""
   homepage ""
-  version "0.4.0"
+  version "0.4.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.4.0/kpexec_v0.4.0_Darwin_arm64.tar.gz"
-      sha256 "e30ec9187e493c1bd8d260c93bf36d239d7f5729d055ef4a45500a0fc28397a7"
+      url "https://github.com/ssup2/kpexec/releases/download/v0.4.1/kpexec_v0.4.1_Darwin_arm64.tar.gz"
+      sha256 "b65aa081e5d939f2026fd297604b70429578380e05711247c7f0bb4533a44b53"
 
       def install
         bin.install "kpexec"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.4.0/kpexec_v0.4.0_Darwin_amd64.tar.gz"
-      sha256 "427f26f3c3666a10ad99d0409c0a1ede0aad47a41404da4dcfff7dcc23c4ddd0"
+      url "https://github.com/ssup2/kpexec/releases/download/v0.4.1/kpexec_v0.4.1_Darwin_amd64.tar.gz"
+      sha256 "5401e210b63f6b4aa91bfe01b151b4971dc2f5ff6c15a6d695c576351ebeaf99"
 
       def install
         bin.install "kpexec"
@@ -28,16 +28,16 @@ class Kpexec < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.4.0/kpexec_v0.4.0_Linux_arm64.tar.gz"
-      sha256 "1e4c2c32a28d4f9a35f20eabbd4ae0557c02ce2dada32df7e2017dcee34c7649"
+      url "https://github.com/ssup2/kpexec/releases/download/v0.4.1/kpexec_v0.4.1_Linux_arm64.tar.gz"
+      sha256 "8c630ae592200a817a3afc6865fee01413dc0055df343428cd987484396d1554"
 
       def install
         bin.install "kpexec"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.4.0/kpexec_v0.4.0_Linux_amd64.tar.gz"
-      sha256 "0ede1fdc3885a6cee92ab80294c51ca617f8bf211887995e8a8376a0aaec03b5"
+      url "https://github.com/ssup2/kpexec/releases/download/v0.4.1/kpexec_v0.4.1_Linux_amd64.tar.gz"
+      sha256 "015793e1fff108df991f9da7a634476b5063fdc54e503bbaa7a146241a859434"
 
       def install
         bin.install "kpexec"
