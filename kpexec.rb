bff42bd4e28f9848d915c0c43a7114221f7359b8
goreleaserbot <goreleaser@carlosbecker.com>
2025-12-05 00:27:39 +0900

    Brew formula update for kpexec version v0.4.2

--- kpexec.rb
+++ kpexec.rb
@@ -5,20 +5,12 @@
 class Kpexec < Formula
   desc ""
   homepage ""
-  version "0.4.1"
+  version "0.4.2"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.4.1/kpexec_v0.4.1_Darwin_arm64.tar.gz"
-      sha256 "b65aa081e5d939f2026fd297604b70429578380e05711247c7f0bb4533a44b53"
-
-      def install
-        bin.install "kpexec"
-      end
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.4.1/kpexec_v0.4.1_Darwin_amd64.tar.gz"
-      sha256 "5401e210b63f6b4aa91bfe01b151b4971dc2f5ff6c15a6d695c576351ebeaf99"
+      url "https://github.com/ssup2/kpexec/releases/download/v0.4.2/kpexec_v0.4.2_Darwin_amd64.tar.gz"
+      sha256 "9db2b956ef930ff63c8fec083d69e9460344096cbaec4a6553e04813a973823e"
 
       def install
         bin.install "kpexec"
@@ -28,16 +20,16 @@ class Kpexec < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.4.1/kpexec_v0.4.1_Linux_arm64.tar.gz"
-      sha256 "8c630ae592200a817a3afc6865fee01413dc0055df343428cd987484396d1554"
+      url "https://github.com/ssup2/kpexec/releases/download/v0.4.2/kpexec_v0.4.2_Linux_arm64.tar.gz"
+      sha256 "ee602ae7f3165e91ce1f1db3b2d46407e705e78778b091e0c9f71419a724c414"
 
       def install
         bin.install "kpexec"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ssup2/kpexec/releases/download/v0.4.1/kpexec_v0.4.1_Linux_amd64.tar.gz"
-      sha256 "015793e1fff108df991f9da7a634476b5063fdc54e503bbaa7a146241a859434"
+      url "https://github.com/ssup2/kpexec/releases/download/v0.4.2/kpexec_v0.4.2_Linux_amd64.tar.gz"
+      sha256 "eb98097b0bd3a9f7b2fa07ed0bda81ab7c547cae660d98c23b9ff946e68ef085"
 
       def install
         bin.install "kpexec"
